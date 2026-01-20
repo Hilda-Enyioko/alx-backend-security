d5fe79eae66d483e7f2b3361ac3d7ddab7748ee9
Hilda Enyioko <enyiokohilda@gmail.com>
2026-01-20 20:12:30 +0100

    store ip geolocations

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -23,3 +23,31 @@ class IPBlockMiddleware(MiddlewareMixin):
         # Check if the IP is blocked
         if BlockedIP.objects.filter(ip_address=ip_address).exists():
             return HttpResponseForbidden("Your IP address has been blocked.")
+        
+
+# A middleware to populate geolocation data for incoming requests based on IP address.
+import ipinfo
+
+class GeoLocationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        ip_address = get_client_ip(request)
+
+        if not ip_address:
+            return
+
+        # Only enrich if this IP has not been geo-resolved before
+        log = RequestLog.objects.filter(ip_address=ip_address).first()
+
+        if log and log.country and log.city:
+            return  # already populated
+
+        handler = ipinfo.getHandler('YOUR_IPINFO_TOKEN')
+        details = handler.getDetails(ip_address)
+
+        country = details.country
+        city = details.city 
+
+        if log:
+            log.country = country
+            log.city = city
+            log.save()
